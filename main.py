9cdb265fe3614e48391a1243a2ebbf1a2c58cbf7
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-08-01 22:26:31 +0200

    .

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run('TOKEN')
+bot.run('NzkwOTY1NDE5NjcwMjQxMjgx.X-IR8w.COGlYLaQEDQP6kZCB2EHYn4nhx4')
