8e7bafed7563fe9f25c8533c1446cc452766fd31
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-08-13 11:41:08 +0200

    .

--- cogs/music.py
+++ cogs/music.py
@@ -93,28 +93,6 @@ class MusicBot(commands.Cog):
         ctx.voice_client.volume = volume / 100
         await ctx.send(f'Volume of the player set to {volume}%')
 
-    @commands.command()
-    async def raft(self, ctx):
-        if ctx.author.voice is None:
-            embed = discord.Embed(title='<:close:864599591692009513> **ERROR**',
-                                  description=f'You\'re not in a voice channel!')
-            await ctx.send(embed=embed)
-        voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
-
-        vc = ctx.voice_client
-        vc.play(discord.FFmpegPCMAudio(
-            'C:/Users/simon/Videos/4K Video Downloader/Raft/RAFT 1.01 Official Soundtrack   Full.mp3'))
-        embed = discord.Embed(title='',
-                              description='Now playing the **Raft theme song!**',
-                              color=discord.Color.dark_blue())
-        embed.set_image(url='https://cdn.discordapp.com/attachments/864513088396591156/875455286368948235/raftpng.png')
-        await ctx.send(embed=embed)
-        await ctx.voice_client.loop()
-
 
 def setup(client):
     client.add_cog(MusicBot(client)
---
+++ cogs/raft.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+
+class Raft(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def raft(self, ctx):
+        if ctx.author.voice is None:
+            embed = discord.Embed(title='<:close:864599591692009513> **ERROR**',
+                                  description=f'You\'re not in a voice channel!')
+            await ctx.send(embed=embed)
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+        vc = ctx.voice_client
+        vc.play(discord.FFmpegPCMAudio(
+            'C:/Users/simon/Videos/4K Video Downloader/Raft/RAFT 1.01 Official Soundtrack   Full.mp3'))
+        embed = discord.Embed(title='',
+                              description='Now playing the **Raft theme song!**',
+                              color=discord.Color.dark_blue())
+        embed.set_image(url='https://cdn.discordapp.com/attachments/864513088396591156/875455286368948235/raftpng.png')
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Raft(client))
