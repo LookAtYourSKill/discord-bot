11bafc33b70abf6c2bf7f61909a6197e070a08e6
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-11-24 21:08:21 +0100

    .

--- cogs/afk command.py
+++ cogs/afk command.py
@@ -7,22 +7,6 @@ class AFK(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='afk')
-    async def afk(self, ctx, mins, reason=None):
-        current_nick = ctx.author.nick
-        await ctx.send(f"{ctx.author.mention} ist für {mins} Afk, wegen {reason}")
-        await ctx.author.edit(nick=f"[AFK] {ctx.author.mention}")
-
-        counter = 0
-        while counter <= int(mins):
-            counter += 1
-            await asyncio.sleep(60)
-
-            if counter == int(mins):
-                await ctx.author.edit(nick=current_nick)
-                await ctx.send(f"{ctx.authot.mention} ist nicht mehr Afk")
-                break
-
 
 def setup(bot):
     bot.add_cog(AFK(bot))
