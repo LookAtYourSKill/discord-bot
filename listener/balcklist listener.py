fb7a593ab6081a0a429468b175da4bb87759f278
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-12-12 17:17:11 +0100

    .

--- listener/balcklist listener.py
+++ listener/balcklist listener.py
@@ -12,11 +12,15 @@ class blacklistListener(commands.Cog):
         with open("./utils/json/blacklist.json", 'r') as f:
             data = json.load(f)
         for bad_word in data["blacklist"]:
-            if bad_word in message.content.lower():
+            if bad_word in message.content:
                 await message.delete()
                 embed = discord.Embed(title='<:close:864599591692009513> **ERROR**',
                                       description='Deine Message `wurde gelöscht`, da du ein Wort `aus der Blacklist darin hattest!`\n**Bitte unterlasse dies!**')
                 await message.channel.send(embed=embed, delete_after=5)
+                with open('../config.json', 'r') as config_file:
+                    config = json.load(config_file)
+                log_channel = config['moderation_log_channel']
+                await log_channel.send(f'{message.author.mention} hat ein Wort aus der Blacklist geschickt!')
 
 
 def setup(bot):
