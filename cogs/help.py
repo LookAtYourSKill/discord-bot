d23a022f57c6723c722769652d5fe2a9490e116d
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-01-02 02:11:49 +0100

    Some Embed things

--- cogs/help.py
+++ cogs/help.py
@@ -1,20 +1,5 @@
-import os
-
 import discord
 from discord.ext import commands
-from discord.errors import Forbidden
-
-
-async def send_embed(ctx, embed):
-    try:
-        await ctx.send(embed=embed)
-    except Forbidden:
-        try:
-            await ctx.send("Hey, seems like I can't send embeds. Please check my permissions :)")
-        except Forbidden:
-            await ctx.author.send(
-                f"Hey, seems like I can't send any message in {ctx.channel.name} on {ctx.guild.name}\n"
-                f"May you inform the server team about this issue? :slight_smile: ", embed=embed)
 
 
 class Help(commands.Cog):
@@ -26,8 +11,10 @@ class Help(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def help(self, ctx, *content):
-        """Shows all modules of that bot"""
+    async def help3(self, ctx, *content):
+        """
+                Shows this help
+                """
 
         prefix = '?'
         version = 1
@@ -36,62 +23,100 @@ class Help(commands.Cog):
         owner_name = 'LookAtYourSkill#6822'
 
         if not content:
-            try:
-                owner = ctx.guild.get_member(owner).mention
+            embed = discord.Embed(title='Commands and Modules', color=discord.Color.blue(),
+                                  description=f'Use `{prefix}help <module>` to gain more information about that module and \n'
+                                              f'`{prefix}help <command>` to gain more information about that command')
 
-            except AttributeError as e:
-                owner = owner
-
-            emb = discord.Embed(title='Commands and modules', color=discord.Color.blue(),
-                                description=f'Use `{prefix}help <module>` to gain more information about that module ')
-
-            cogs_desc = ''
-            for cog in self.bot.cogs:
-                cogs_desc += f'- `{cog}`\n'  # {self.bot.cogs[cog].__doc__}\n'
-
-            emb.add_field(name='Modules', value=cogs_desc, inline=False)
-
-            commands_desc = ''
-            for command in self.bot.walk_commands():
-                if not command.cog_name and not command.hidden:
-                    commands_desc += f'{command.name} - {command.help}\n'
-                    print(commands_desc)
-
-            if commands_desc:
-                emb.add_field(name='Not belonging to a module', value=commands_desc, inline=False)
-
-            emb.add_field(name="About", value=f"The Bots is developed by {owner_name}, based on discord.py.\n"
-                                              f"This version of it is maintained by {owner}")
+            embed.add_field(name='Moderation',
+                            value='`ban`, `softban`, `unban`, `unbanall`, `tempban`, `banned`, `mute`, `unmute`, `tempmute`, `kick`, `dc`, `clear`, `slowmode`, `nuke`, `block`, `unblock`',
+                            inline=False)
+            embed.add_field(name='Blacklist',
+                            value='`blacklistadd`, `blacklistremove`, `blacklistshow`, `blacklistclear`',
+                            inline=False)
+            embed.add_field(name=f'Administration',
+                            value=f'`setstatus`, `lock`, `release`, `say`, `gawrole`, `muterole`, `rules`',
+                            inline=False)
+            embed.add_field(name='Utilities',
+                            value='`invite`, `invites`, `botinvite`, `embed`, `repeat`, `senddm`, `password`, `spotify`, `add`, `minus`, `multiplicate`, `divide`',
+                            inline=False)
+            embed.add_field(name='Channel',
+                            value='`deletechannel`, `createchannel`, `tempchannel`',
+                            inline=False)
+            embed.add_field(name='Roles',
+                            value='`giverole`, `removerole`, `create_role`, `delete_role`, `getroles`, `removeroles`',
+                            inline=False)
+            embed.add_field(name='Fun',
+                            value='`8ball`, `slots`, `dice`, `rps`, `num_game`, `simp`, `sus`',
+                            inline=False)
+            embed.add_field(name='Info',
+                            value='`info`, `members`, `server`, `joined`, `bot`, `avatar`',
+                            inline=False)
+            embed.add_field(name='Systems',
+                            value='`verify`, `setup`, `toggle`, `new`, `close`, `addaccess`, `delaccess`, `addpingedrole`, `delpingedrole`, `addadminrole`, `deladminrole`')
+            embed.add_field(name='Timers',
+                            value='`reminder`, `giveaway`',
+                            inline=False)
+            await ctx.send(embed=embed)
 
         elif len(content) == 1:
-
-            for cog in self.bot.cogs:
-                if cog.lower() == content[0].lower():
-
-                    emb = discord.Embed(title=f'{cog} - Commands', description=self.bot.cogs[cog].__doc__,
-                                        color=discord.Color.green())
-
-                    for command in self.bot.get_cog(cog).get_commands():
-                        if not command.hidden:
-                            emb.add_field(name=f"`{prefix}{command.name}`", value=command.help, inline=False)
-                    break
-
-            else:
-                emb = discord.Embed(title="What's that?!",
-                                    description=f"I've never heard from a module called `{content[0]}` before ",
-                                    color=discord.Color.orange())
+            embed = discord.Embed(
+                description=f'Verwende `{prefix}help <command>` to gain more information about the command',
+                color=discord.Color.blurple())
+            embed.add_field(name='ban <member> [reason]',
+                            value='Ban a member from your server',
+                            inline=False)
+            embed.add_field(name='softban <member> [reason]',
+                            value='Kick a member from your server and delete all of its messages',
+                            inline=False)
+            embed.add_field(name='unban <id>',
+                            value='Unban a member from your server',
+                            inline=False)
+            embed.add_field(name='unbanall',
+                            value='Unban every banned member on your server',
+                            inline=False)
+            embed.add_field(name='tempban <member> <time> [reason]',
+                            value='Ban a member for a specific time from your server',
+                            inline=False)
+            embed.add_field(name='banned',
+                            value='List all banned members',
+                            inline=False)
+            embed.add_field(name='mute <member>',
+                            value='Give a member a specific role, so the user cant write in any textchannel\'s and join in any voicechannel\'s',
+                            inline=False)
+            embed.add_field(name='unmute <member>',
+                            value='Remove a specific role from a member, so the user can write again in any textchannel\'s and join in any voicechannel\'s',
+                            inline=False)
+            embed.add_field(name='tempmute <member> <time> [reason]',
+                            value='Give a member a specific role, so the user cant write in any textchannel\'s and join in any voicechannel\'s for a specific time',
+                            inline=False)
+            embed.add_field(name='kick <member>',
+                            value='Kick a user from your server',
+                            inline=False)
+            embed.add_field(name='dc <member>',
+                            value='Disconnect a user from a voicechannel',
+                            inline=False)
+            embed.add_field(name='clear <amount>',
+                            value='Clears a specific amount of messages',
+                            inline=False)
+            embed.add_field(name='slowmode <seconds>',
+                            value='Activate / Disable a slowmode in a textchannel',
+                            inline=False)
+            embed.add_field(name='nuke <channel>',
+                            value='Delete and Clone a channel on your server',
+                            inline=False)
+            embed.add_field(name='block <member>',
+                            value='Block a user from writing in a textchannel',
+                            inline=False)
+            embed.add_field(name='unblock <member>',
+                            value='Unblock a user from writing in a textchannel',
+                            inline=False)
+            await ctx.send(embed=embed)
 
         elif len(content) > 1:
-            emb = discord.Embed(title="That's too much.",
-                                description="Please request only one module at once ",
-                                color=discord.Color.orange())
-
-        else:
-            emb = discord.Embed(title="It's a magical place.",
-                                description="I don't know how you got here. But I didn't see this coming at all.",
-                                color=discord.Color.red())
-
-        await send_embed(ctx, emb)
+            embed = discord.Embed(title="That's too much.",
+                                  description="Please request only one module/command at once ",
+                                  color=discord.Color.orange())
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
