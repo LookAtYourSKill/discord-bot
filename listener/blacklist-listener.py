02173196886f142d6166234ffb8fcf9252620b76
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-01-15 00:37:04 +0100

    Added channel blacklist

--- listener/blacklist-listener.py
+++ listener/blacklist-listener.py
@@ -9,16 +9,20 @@ class blacklistListener(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+
         with open("./utils/json/blacklist.json", 'r') as f:
             data = json.load(f)
+
         for bad_word in data["blacklist"]:
             if bad_word in message.content:
                 await message.delete()
                 embed = discord.Embed(title='<:close:864599591692009513> **ERROR**',
                                       description='Deine Message `wurde gelöscht`, da du ein Wort `aus der Blacklist darin hattest!`\n**Bitte unterlasse dies!**')
                 await message.channel.send(embed=embed, delete_after=5)
+
                 with open('../etc/config.json', 'r') as config_file:
                     config = json.load(config_file)
+
                 log_channel = config['moderation_log_channel']
                 await log_channel.send(f'{message.author.mention} hat ein Wort aus der Blacklist geschickt!')
 
