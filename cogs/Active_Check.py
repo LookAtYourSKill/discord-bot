d5fcfb9659e934e754b093412b5917c4295d69e6
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-01-22 02:33:05 +0100

    Added method to check if a file is active or not

---
+++ cogs/Active_Check.py
@@ -0,0 +1,177 @@
+import json
+
+import discord
+from discord.ext import commands
+
+
+class Active_Check(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def activate(self, ctx, cog):
+        with open('utils/json/active_check.json', 'r') as f:
+            data = json.load(f)
+
+        if str(ctx.guild.id) not in data:
+            new_checks = {
+                "Administration": "true",
+                "Automod": "true",
+                "Channel": "true",
+                "Fun": "true",
+                "Help": "true",
+                "Info": "true",
+                "Math": "true",
+                "Moderation": "true",
+                "Music": "true",
+                "Poll": "true",
+                "Roles": "true",
+                "Rules": "true",
+                "Setup": "true",
+                "Ticket_System": "true",
+                "Timers": "true",
+                "Translator": "true",
+                "Utilities": "true",
+                "Verify": "true"
+            }
+            data[str(ctx.guild.id)] = new_checks
+            with open('utils/json/active_check.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'true':
+            embed = discord.Embed(description=f'Das `Modul {cog}` ist **bereits aktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'false':
+
+            data[str(ctx.guild.id)][f"{cog}"] = 'true'
+            with open('utils/json/active_check.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+            embed = discord.Embed(description=f'Das `Modul {cog}` **war deaktiviert** und wurde **nun aktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(description=f'**Unbekannter Fehler!** Versuche es in ein paar Sekunden erneut',
+                                  color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def deactivate(self, ctx, cog):
+        with open('utils/json/active_check.json', 'r') as f:
+            data = json.load(f)
+
+        if str(ctx.guild.id) not in data:
+            new_checks = {
+                "Administration": "true",
+                "Automod": "true",
+                "Channel": "true",
+                "Fun": "true",
+                "Help": "true",
+                "Info": "true",
+                "Math": "true",
+                "Moderation": "true",
+                "Music": "true",
+                "Poll": "true",
+                "Roles": "true",
+                "Rules": "true",
+                "Setup": "true",
+                "Ticket_System": "true",
+                "Timers": "true",
+                "Translator": "true",
+                "Utilities": "true",
+                "Verify": "true"
+            }
+            data[str(ctx.guild.id)] = new_checks
+            with open('utils/json/active_check.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'false':
+            embed = discord.Embed(description=f'Das `Modul {cog}` ist **bereits deaktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'true':
+
+            data[str(ctx.guild.id)][f"{cog}"] = 'false'
+            with open('utils/json/active_check.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+            embed = discord.Embed(description=f'Das `Modul {cog}` **war aktiviert** und wurde **nun deaktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(description=f'**Unbekannter Fehler!** Versuche es in ein paar Sekunden erneut',
+                                  color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def check(self, ctx, cog):
+        with open('utils/json/active_check.json', 'r') as f:
+            data = json.load(f)
+
+        if str(ctx.guild.id) not in data:
+            new_checks = {
+                "Administration": "true",
+                "Automod": "true",
+                "Channel": "true",
+                "Fun": "true",
+                "Help": "true",
+                "Info": "true",
+                "Math": "true",
+                "Moderation": "true",
+                "Music": "true",
+                "Poll": "true",
+                "Roles": "true",
+                "Rules": "true",
+                "Setup": "true",
+                "Ticket_System": "true",
+                "Timers": "true",
+                "Translator": "true",
+                "Utilities": "true",
+                "Verify": "true"
+            }
+            data[str(ctx.guild.id)] = new_checks
+            with open('utils/json/active_check.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'false':
+            embed = discord.Embed(description=f'Das Modul `{cog}` ist **momentan deaktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        elif data[str(ctx.guild.id)][f"{cog}"] == 'true':
+            embed = discord.Embed(description=f'Das Modul `{cog}` ist **momentan aktiviert!**',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        else:
+            embed = discord.Embed(description=f'**Unbekannter Fehler!** Versuche es in ein paar Sekunden erneut',
+                                  color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    async def check_all(self, ctx):
+        with open('utils/json/active_check.json', 'r') as f:
+            data = json.load(f)
+
+        if str(ctx.guild.id) not in data:
+            embed = discord.Embed(
+                description=f'Dein Server ist anscheinend noch nicht registriert... Führe bitte zuerst den Command `?activate Moderation`, um den Server eintragen zu lassen!',
+                color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+        elif str(ctx.guild.id) in data:
+            embed = discord.Embed(description=f'{data[str(ctx.guild.id)]}',
+                                  color=discord.Color.green())
+            await ctx.send(embed=embed)
+
+        else:
+            return
+
+
+def setup(bot):
+    bot.add_cog(Active_Check(bot))
