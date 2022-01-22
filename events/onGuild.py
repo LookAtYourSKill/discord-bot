e53c2e52495f244b181df2a9a575bf3482895727
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-01-22 16:42:23 +0100

    Create more start values for a new guild

--- events/onGuild.py
+++ events/onGuild.py
@@ -20,7 +20,7 @@ class on_guild(commands.Cog):
             "VERIFY_CHANNEL": (),
             "BOT_ROLE": (),
             "VERIFIED_ROLE": ()
-            }
+        }
 
         data[str(guild.id)] = new_server
         with open('utils/json/on_guild.json', 'w') as f:
@@ -65,25 +65,42 @@ class on_guild(commands.Cog):
         with open('utils/json/active_check.json', 'w') as f:
             json.dump(data, f, indent=4)
 
-    @commands.Cog.listener()
-    async def on_guild_leave(self, guild):
-        with open('utils/json/on_guild.json', 'r') as f:
+        with open('utils/json/temp_times.json', 'r') as f:
             data = json.load(f)
 
-        old_server = {
-            "SERVERNAME": f"{guild.name}",
-            "MESSAGE_LOG_CHANNEL": (),
-            "MODERATION_LOG_CHANNEL": (),
-            "WELCOME_CHANNEL": (),
-            "LEAVE_CHANNEL": (),
-            "VERIFY_CHANNEL": (),
-            "BOT_ROLE": (),
-            "VERIFIED_ROLE": ()
+        new_tempmute_times = {
+            "tempmutes": {
+
+            },
+            "tempbans": {
+
             }
-        data[str(guild.id)].remove(old_server)
-        with open('utils/json/on_guild.json', 'w') as f:
+        }
+
+        data[str(guild.id)] = new_tempmute_times
+        with open('utils/json/temp_times.json', 'w') as f:
             json.dump(data, f, indent=4)
 
+        with open('utils/json/blacklist.json', 'r') as f:
+            data = json.load(f)
+
+        new_blacklist = {
+            "blacklist": [
+
+            ],
+            "channel_blacklist": [
+
+            ]
+        }
+
+        data[str(guild.id)] = new_blacklist
+        with open('utils/json/blacklist.json', 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+@commands.Cog.listener()
+async def on_guild_leave(self, guild):
+    pass
 
 def setup(bot):
     bot.add_cog(on_guild(bot))
