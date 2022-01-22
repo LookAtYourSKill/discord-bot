801ce7f443b2cbe75e4c27bb8138e8bc582bd604
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-01-22 16:12:50 +0100

    Improved Blacklist Listener

--- listener/blacklist-listener.py
+++ listener/blacklist-listener.py
@@ -10,21 +10,23 @@ class blacklistListener(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        with open("./utils/json/blacklist.json", 'r') as f:
+        with open("utils/json/blacklist.json", 'r') as f:
             data = json.load(f)
 
-        for bad_word in data["blacklist"]:
+        for bad_word in data[str(message.guild.id)]["blacklist"]:
             if bad_word in message.content:
                 await message.delete()
                 embed = discord.Embed(title='<:close:864599591692009513> **ERROR**',
                                       description='Deine Message `wurde gelöscht`, da du ein Wort `aus der Blacklist darin hattest!`\n**Bitte unterlasse dies!**')
                 await message.channel.send(embed=embed, delete_after=5)
 
-                with open('../etc/config.json', 'r') as config_file:
+                with open('./etc/config.json', 'r') as config_file:
                     config = json.load(config_file)
 
-                log_channel = config['moderation_log_channel']
-                await log_channel.send(f'{message.author.mention} hat ein Wort aus der Blacklist geschickt!')
+                log_channel = self.bot.get_channel(id=config["moderation_log_channel"])
+                embed = discord.Embed(description=f'{message.author} hat ein Wort aus der Blacklist geschickt!',
+                                      color=discord.Color.red())
+                await log_channel.send(embed=embed)
 
 
 def setup(bot):
